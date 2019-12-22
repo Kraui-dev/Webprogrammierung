803ebdea5c50d68f4f29df848438eb2577e4b3b2
Christoph Enke <110046@fhwn.ac.at>
2019-12-22 23:54:27 +0100

    changes

--- Hausaufgabe_2/gulpfile.js
+++ Hausaufgabe_2/gulpfile.js
@@ -1,18 +1,7 @@
 const { src, dest, parallel } = require('gulp');
 const htmlhint = require('gulp-htmlhint');
 const debug = require('gulp-debug');
-// const eslint = require('gulp-eslint');
 const uncomment = require('gulp-uncomment');
-// var tslint = require('gulp-tslint');
-
-// async function tsLint() {
-//     return src('*.ts')
-//         .pipe(tslint({
-//             formatter: 'verbose'
-//         }))
-//         .pipe(tslint.report())
-//         .pipe(dest('./dist'));
-// }
 
 async function html() {
     return src('*.html')
@@ -36,17 +25,7 @@ async function copyLibs() {
         .pipe(dest('./dist'));
 }
 
-// async function esLint() {
-//     return src(['ts/*.js'])
-//         .pipe(eslint())
-//         .pipe(eslint.format())
-//         .pipe(eslint.failAfterError());
-// }
-
-// exports.tslint = tsLint;
-// exports.eslint = esLint;
 exports.html = html;
 exports.uncomment = unComment;
 exports.copyLibs = copyLibs;
 exports.default = parallel(html, copyLibs, unComment);
-// exports.default = parallel(html, esLint, copyLibs, unComment);
