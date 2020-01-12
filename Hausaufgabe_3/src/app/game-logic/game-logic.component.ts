7b57689f92260c035d6f4fdcd7f85775f118f87b
Christoph Enke <110046@fhwn.ac.at>
2020-01-12 09:51:32 +0100

    2d gamefield added

--- Hausaufgabe_3/src/app/game-logic/game-logic.component.ts
+++ Hausaufgabe_3/src/app/game-logic/game-logic.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
-import { Stage } from 'stage-js/platform/web';
+// import { join } from 'path';
+// import { Stage } from 'stage-js/platform/web';
 
 declare var stageAnimation: any;
 
@@ -12,7 +13,8 @@ export class GameLogicComponent implements OnInit {
 
   @ViewChild('gameCanvas', { static: true })
   canvas: ElementRef<HTMLCanvasElement>;
- 
+  matrixField: number[][];
+
   private ctx: CanvasRenderingContext2D;
 
   constructor() { }
@@ -25,20 +27,101 @@ export class GameLogicComponent implements OnInit {
 // this.ctx.rect(5, 5, 290, 140);
 // this.ctx.stroke();
 // this.ctx.beginPath();
-        // for(let i =0;i<28;i++){
-        //   this.ctx.fillStyle = i%2===0?"green":"blue";
-        //   this.ctx.fillRect(10*i + 2, 0, 10, 10);
-        //   this.ctx.stroke();
-        // }
+//         for(let i =0;i<28;i++){
+//           this.ctx.fillStyle = i%2===0?"green":"blue";
+//           this.ctx.fillRect(10*i + 2, 0, 10, 10);
+//           this.ctx.stroke();
+//         }
         
-        let dot = new Image();
-        dot.src = '../../assets/dot.png';
-        dot.width = 28;
-        dot.height = 31;
-        dot.onload = () => {
-          this.ctx.drawImage(dot, 10 + 2, 10 - 2);
+this.create2DField();
+
+//         let dot = new Image();
+//         dot.src = '../../assets/dot.png';
+//         dot.width = 28;
+//         dot.height = 31;
+//         dot.onload = () => {
+//           this.ctx.drawImage(dot, 10 + 2, 20 - 2);
+//         };
+//         dot.onload = () => {
+//           this.ctx.drawImage(dot, 200 + 2, 20 - 2);
+//         };
+//         dot.onload = () => {
+//           this.ctx.drawImage(dot, 20 + 2, 20 - 2);
+//         };
+// let ghost = new Image();
+// ghost.src = '../../assets/ghostpink.png';
+// ghost.width = 28;
+// ghost.height = 31;
+// ghost.onload = () => {
+//           this.ctx.drawImage(ghost, 30 + 2, 10 - 2);
+//         }
+
+        for(let i = 0; i < 31;i++){
+          for(let j = 0;j < 28; j++){
+            
+            if (this.matrixField[i][j] == 1){
+              let img = new Image();
+              img.src = '../../assets/pellet.png';
+              img.width = 28;
+              img.height = 31;
+              img.onload = () => {
+                this.ctx.drawImage(img, j * 10 + 5,  i * 10 + 2);
+            }
+          }
+
+          if (this.matrixField[i][j] == 2){
+            let img = new Image();
+            img.src = '../../assets/test_pellet.png';
+            img.width = 28;
+            img.height = 31;
+            img.onload = () => {
+              this.ctx.drawImage(img, j * 10 + 5,  i * 10 + 2);
+          }
         }
 
+            if (this.matrixField[i][j] == 3){
+              let img = new Image();
+              img.src = '../../assets/pink_ghost.png';
+              img.width = 28;
+              img.height = 31;
+              img.onload = () => {
+                this.ctx.drawImage(img, j * 10 + 5,  i * 10 + 2);
+            }
+          }
+
+            if (this.matrixField[i][j] == 4){
+              let img = new Image();
+              img.src = '../../assets/red_ghost.png';
+              img.width = 28;
+              img.height = 31;
+              img.onload = () => {
+                this.ctx.drawImage(img, j * 10 + 5,  i * 10 + 2);
+            }
+          }
+
+            if (this.matrixField[i][j] == 5){
+              let img = new Image();
+              img.src = '../../assets/orange_ghost.png';
+              img.width = 28;
+              img.height = 31;
+              img.onload = () => {
+                this.ctx.drawImage(img, j * 10 + 5,  i * 10 + 2);
+            }
+          }
+
+            if (this.matrixField[i][j] == 6){
+              let img = new Image();
+              img.src = '../../assets/turquoise_ghost.png';
+              img.width = 28;
+              img.height = 31;
+              img.onload = () => {
+                this.ctx.drawImage(img, j * 10 + 5,  i * 10 + 2);
+            }
+          }
+            
+          }
+        }
+      
         // const image = new Image(28, 31);
         // image.onload = this.drawImageActualSize;
         // dotImage.widh
@@ -53,6 +136,42 @@ export class GameLogicComponent implements OnInit {
         //   }
         // }
 // this.loadBackground();
+  }
+
+  create2DField(){
+    this.matrixField = [
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0],
+      [0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0],
+      [0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0],
+      [0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0],
+      [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+      [0,1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0],
+      [0,1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0],
+      [0,1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,0],
+      [0,0,0,0,0,0,1,0,0,0,0,0,2,0,0,2,0,0,0,0,0,1,0,0,0,0,0,0],
+      [0,0,0,0,0,0,1,0,0,0,0,0,2,0,0,2,0,0,0,0,0,1,0,0,0,0,0,0],
+      [0,0,0,0,0,0,1,0,0,2,2,2,2,2,2,2,2,2,2,0,0,1,0,0,0,0,0,0],
+      [0,0,0,0,0,0,1,0,0,2,0,0,0,2,2,0,0,0,2,0,0,1,0,0,0,0,0,0],
+      [0,0,0,0,0,0,1,0,0,2,0,2,3,4,5,6,2,0,2,0,0,1,0,0,0,0,0,0],
+      [2,2,2,2,2,2,1,2,2,2,0,0,0,0,0,0,0,0,2,2,2,1,2,2,2,2,2,2],
+      [0,0,0,0,0,0,1,0,0,2,0,0,0,0,0,0,0,0,2,0,0,1,0,0,0,0,0,0],
+      [0,0,0,0,0,0,1,0,0,2,0,0,0,0,0,0,0,0,2,0,0,1,0,0,0,0,0,0],
+      [0,0,0,0,0,0,1,0,0,2,2,2,2,2,2,2,2,2,2,0,0,1,0,0,0,0,0,0],
+      [0,0,0,0,0,0,1,0,0,2,0,0,0,0,0,0,0,0,2,0,0,1,0,0,0,0,0,0],
+      [0,0,0,0,0,0,1,0,0,2,0,0,0,0,0,0,0,0,2,0,0,1,0,0,0,0,0,0],
+      [0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0],
+      [0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0],
+      [0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0],
+      [0,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,0],
+      [0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0],
+      [0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0],
+      [0,1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,0],
+      [0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0],
+      [0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0],
+      [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    ];
   }
 
   drawImageActualSize(){
